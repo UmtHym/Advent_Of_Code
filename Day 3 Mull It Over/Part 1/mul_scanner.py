297b38a4d78ae7f97acc51b3e310260cb9011378
UmtHym <120595657+UmtHym@users.noreply.github.com>
2025-01-29 12:43:28 +0100

    Day 3 Part 1 Done!

---
+++ Day 3 Mull It Over/Part 1/mul_scanner.py
@@ -0,0 +1,18 @@
+import re
+fhand = open("memory.txt")
+
+total = 0
+for line in fhand:
+    line = line.strip()
+    lst = re.findall(r'mul\(\d{1,3},\d{1,3}\)', line)
+    
+    for element in lst:
+        number1 = int(re.findall(r'mul\((\d{1,3})', element)[0])
+        number2 = int(re.findall(r'\,(\d{1,3})\)', element)[0])
+        total += (number1 * number2)
+
+print(total)
+
+  
+
+
